1a4c54aaa2f4c21190836d86886685dc1f66e1d7
Bora Özışık <ozisikbora@gmail.com>
2022-06-30 17:48:10 +0300

    navbar recreating...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,8 @@ import {
   Grid,
   Stack,
   InputAdornment,
+  Tabs,
+  Tab,
 } from "@mui/material";
 
 import SearchIcon from "@mui/icons-material/Search";
@@ -94,9 +96,289 @@ const Navbar = ({ searchMovies, setSearchValue, setMovies, selectMovie }) => {
   };
 
   return (
-    <AppBar style={{ color: "white", backgroundColor: "#EC7063", height: 80 }}>
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Grid sx={{ p: 2 }}>
+    <>
+      <AppBar sx={{ backgroundColor: "#EC7063" }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit">
+            <MovieFilterSharpIcon />
+          </IconButton>
+          <Typography>ZBD Movies</Typography>
+          <Stack sx={{ marginLeft: "auto" }}>
+            <form onSubmit={searchMovies}>
+              <Stack direction="row">
+                <TextField
+                  id="search-text"
+                  label="Search for movies..."
+                  variant="filled"
+                  size="small"
+                  sx={{ backgroundColor: "white" }}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClearClick}>
+                          <ClearIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  size="medium"
+                  endIcon={<SearchIcon />}
+                  style={{
+                    marginLeft: 10,
+                    height: 48,
+                    borderRadius: "5px 15px 15px 5px",
+                    backgroundColor: "#1F618D",
+                  }}
+                >
+                  Search
+                </Button>
+              </Stack>
+            </form>
+          </Stack>
+
+          <Stack sx={{ marginLeft: "auto" }}>
+            <Button
+              id="filter-movies-button"
+              variant="contained"
+              size="medium"
+              onClick={handleClick}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              endIcon={<KeyboardArrowDownIcon />}
+              sx={{ backgroundColor: "#1F618D", height: 47 }}
+            >
+              Show Movies...
+            </Button>
+            <Menu
+              id="movie-filters"
+              anchorEl={anchorElement}
+              open={open}
+              MenuListProps={{
+                "aria-labelledby": "filter-movies-button",
+              }}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <MenuItem id="in_theatres" onClick={handleClose}>
+                {" "}
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />{" "}
+                In Theatres
+              </MenuItem>
+              <MenuItem id="most_popular_now" onClick={handleClose}>
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Now
+              </MenuItem>
+              <MenuItem id="most_popular_actions_now" onClick={handleClose}>
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Actions Now
+              </MenuItem>
+              <MenuItem id="most_popular_dramas_now" onClick={handleClose}>
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Dramas Now
+              </MenuItem>
+              <MenuItem id="most_popular_crimes_now" onClick={handleClose}>
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Crimes Now
+              </MenuItem>
+              <MenuItem
+                id="most_popular_science-fictions_now"
+                onClick={handleClose}
+              >
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Science-Fictions Now
+              </MenuItem>
+              <MenuItem id="most_popular_comedies_now" onClick={handleClose}>
+                <CircleTwoToneIcon
+                  sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                />
+                Most Popular Comedies Now
+              </MenuItem>
+            </Menu>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </>
+    /*<AppBar position="static" sx={{ backgroundColor: "#EC7063" }}>
+      <Toolbar variant="dense" sx={{ marginTop: 1, marginBottom: 1 }}>
+        <Grid container>
+          <Grid
+            item
+            xs={6}
+            sm={6}
+            md={6}
+            lg={6}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MovieFilterSharpIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              ZBD Movies
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sm={6}
+            md={6}
+            lg={6}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Grid
+              container
+              sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Grid item>
+                <form onSubmit={searchMovies}>
+                  <Stack direction="row">
+                    <TextField
+                      id="search-text"
+                      label="Search for movies..."
+                      variant="filled"
+                      size="small"
+                      sx={{ backgroundColor: "white" }}
+                      onChange={(e) => setSearchValue(e.target.value)}
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton onClick={handleClearClick}>
+                              <ClearIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      size="medium"
+                      endIcon={<SearchIcon />}
+                      style={{
+                        marginLeft: 10,
+                        height: 48,
+                        borderRadius: "5px 15px 15px 5px",
+                        backgroundColor: "#1F618D",
+                      }}
+                    >
+                      Search
+                    </Button>
+                  </Stack>
+                </form>
+              </Grid>
+
+              <Grid item>
+                <Button
+                  id="filter-movies-button"
+                  variant="contained"
+                  size="medium"
+                  onClick={handleClick}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  endIcon={<KeyboardArrowDownIcon />}
+                  sx={{ backgroundColor: "#1F618D", height: 47 }}
+                >
+                  Show Movies...
+                </Button>
+                <Menu
+                  id="movie-filters"
+                  anchorEl={anchorElement}
+                  open={open}
+                  MenuListProps={{
+                    "aria-labelledby": "filter-movies-button",
+                  }}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                >
+                  <MenuItem id="in_theatres" onClick={handleClose}>
+                    {" "}
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />{" "}
+                    In Theatres
+                  </MenuItem>
+                  <MenuItem id="most_popular_now" onClick={handleClose}>
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Now
+                  </MenuItem>
+                  <MenuItem id="most_popular_actions_now" onClick={handleClose}>
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Actions Now
+                  </MenuItem>
+                  <MenuItem id="most_popular_dramas_now" onClick={handleClose}>
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Dramas Now
+                  </MenuItem>
+                  <MenuItem id="most_popular_crimes_now" onClick={handleClose}>
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Crimes Now
+                  </MenuItem>
+                  <MenuItem
+                    id="most_popular_science-fictions_now"
+                    onClick={handleClose}
+                  >
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Science-Fictions Now
+                  </MenuItem>
+                  <MenuItem
+                    id="most_popular_comedies_now"
+                    onClick={handleClose}
+                  >
+                    <CircleTwoToneIcon
+                      sx={{ width: 12, marginRight: 0.6, color: "#EC7063" }}
+                    />
+                    Most Popular Comedies Now
+                  </MenuItem>
+                </Menu>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>*/
+    /*<AppBar style={{ color: "white", backgroundColor: "#EC7063", height: 80 }}>
+      <Toolbar>
+        <Stack sx={{ p: 2 }}>
           <Stack sx={{ p: 2 }} direction="row" spacing={1} alignItems="center">
             <IconButton
               size="large"
@@ -111,7 +393,7 @@ const Navbar = ({ searchMovies, setSearchValue, setMovies, selectMovie }) => {
               </Typography>
             </IconButton>
           </Stack>
-        </Grid>
+        </Stack>
         <Stack direction="row">
           <Grid sx={{ p: 2 }}>
             <form onSubmit={searchMovies}>
@@ -231,7 +513,7 @@ const Navbar = ({ searchMovies, setSearchValue, setMovies, selectMovie }) => {
           </Grid>
         </Stack>
       </Toolbar>
-    </AppBar>
+    </AppBar>*/
   );
 };
 
